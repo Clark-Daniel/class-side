df3da46bd1702cb51bbc15c8e50eb061f7c94bd0
Daniel Clark <clarkdj4@mymail.vcu.edu>
2020-11-29 22:57:22 -0500

    LLEST initial push
    final project "llest" first push.

---
+++ llest/app.js
@@ -0,0 +1,31 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END section
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//ScrollMagic
+const controller = new ScrollMagic.Controller();
+//Scenes
+const scene = new ScrollMagic.Scene({
+    duration: 77000,
+    triggerElement: intro,
+    triggerHook: 0,
+})
+.setPin(intro)
+.addTo(controller);
+
+//Video animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on('update', e => {
+    scrollpos = e.scrollPos / 1000;
+})
+
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    video.currentTime = delay;
+}, 33.3);
